25aaf42bb6f4a8a2d39a8179be2cbeb501440044
Michael Litvin <michael.litvin@gmail.com>
2025-01-29 15:29:45 +0200

    Add Google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER_ID}`}></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_TAG_MANAGER_ID}');
+          `}
+        </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
